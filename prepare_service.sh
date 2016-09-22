6d715a9e0fe06ec12bba8510349cf7ef7d7c5eea
Ayat <ayat.rashad@gmail.com>
2016-09-22 11:23:54 -0400

    modified prep script

--- prepare_service.sh
+++ prepare_service.sh
@@ -7,3 +7,9 @@ pip install -r requirements.txt
 deactivate
 
 mkdir log
+
+# redis-server /etc/redis.conf &
+
+uwsgi --ini uwsgi.ini --socket 127.0.0.1:9090 --pidfile /tmp/an-serv-master1.pid &
+# uwsgi --ini uwsgi.ini --socket 127.0.0.1:9091  --pidfile /tmp/an-serv-master2.pid &
+# uwsgi --ini uwsgi.ini --socket 127.0.0.1:9092  --pidfile /tmp/an-serv-master3.pid &
